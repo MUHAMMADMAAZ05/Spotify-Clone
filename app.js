9ef0bda1e3d45e0494a5c01033482163071722a3
MUHAMMADMAAZ05 <shaikhsiddiqui931@gmail.com>
2026-06-19 11:44:08 -0700

    ok:

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let songIndex = 0;
 let audioElement = new Audio('songs/1.mp3');
-
+audioElement.preload = "auto";
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
